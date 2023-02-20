ac972665c40e83dc3d01efb0cab3645546913d6f
Muhammad Taufik <88309698+xkuadrattt@users.noreply.github.com>
2023-02-20 22:34:43 +0700

    z index problem fixed

--- src/components/SectionFeature.tsx
+++ src/components/SectionFeature.tsx
@@ -3,7 +3,7 @@ import imgfeature from "../assets/img/imgfeature.png";
 
 export default function SectionFeature() {
   return (
-    <section className="flex justify-center mt-8">
+    <section className="flex justify-center mt-8 relative z-[2]">
       <img src={imgfeature} alt="feature image" />
     </section>
   );
